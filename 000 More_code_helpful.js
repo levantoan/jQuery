bbe664150d7f651d0ae9e1ec76f23ac2709a89a4
levantoan <levantoan@users.noreply.github.com>
2016-09-09 10:31:31 +0700

    Update 000 More_code_helpful.js

--- 000 More_code_helpful.js
+++ 000 More_code_helpful.js
@@ -4,3 +4,17 @@ $('.title-img-header').each(function(){
 	    return v.replace(/(\s)(\w+)/, '$1<span>$2</span>');
 	});
 });
+
+//Add span to first word or more word
+$.fn.wrapStart = function (numWords) { 
+    var node = this.contents().filter(function () { return this.nodeType == 3 }).first(),
+        text = node.text(),
+        first = text.split(" ", numWords).join(" ");
+
+    if (!node.length)
+        return;
+    
+    node[0].nodeValue = text.slice(first.length);
+    node.before('<span>' + first + '</span>');
+};
+$("div").wrapStart(1);
