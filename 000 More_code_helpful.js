9c62f76794a874d32c6d074dab4d056dfa78e1e8
levantoan <levantoan@users.noreply.github.com>
2016-09-09 10:11:16 +0700

    Update 000 More_code_helpful.js

--- 000 More_code_helpful.js
+++ 000 More_code_helpful.js
@@ -1,4 +1,6 @@
 //Add span to second word
-$('li.but > a').html(function(i, v) {
-    return v.replace(/\s(.*?)\s/, ' <span>$1</span> ');
+$('.title-img-header').each(function(){
+	$(this).html(function(i, v) {	
+	    return v.replace(/(\s)(\w+)/, '$1<span>$2</span>');
+	});
 });
